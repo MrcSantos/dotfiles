8b5f0b194600c688eb74fc12d5e62853dde0a46d
MrcSantos <mirco.santosuosso@gmail.com>
2023-04-20 23:08:40 +0200

    fit root

--- run_before_000_installations.sh
+++ run_before_000_installations.sh
@@ -2,23 +2,36 @@
 set -eu
 
 OS=$(( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1)
-echo "[.] Enter the sudo password: "
-read -s PASSWORD
+read -p "[.] Enter the sudo password: " -s PASSWORD
+echo ""
+
+
+solus() {
+	echo "[-] Updating system..."
+	eopkg upgrade -y 1>/dev/null
+	
+	echo "[-] Installing basic tools..."
+	eopkg install neofetch git tmux vim -y 1>/dev/null
+
+	echo "[-] Installing nnn..."
+	eopkg install nnn
+
+	echo "[-] Installing ZShell..."
+	eopkg install zsh -y 1>/dev/null
+}
+
+
 
 if [ $OS = '"Solus"' ]
 then
-	echo "[-] Updating system..."
-	echo $PASSWORD | sudo -S eopkg upgrade -y 1>/dev/null
-	
-	echo "[-] Installing basic tools..."
-	echo $PASSWORD | sudo -S eopkg install neofetch git tmux vim -y 1>/dev/null
+	echo $PASSWORD | sudo -S bash -c "$(declare -f solus); solus"
 
-	echo "[-] Installing and configuring ZShell..."
-	echo $PASSWORD | sudo -S eopkg install zsh -y 1>/dev/null
 	echo 'y' | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 1>/dev/null
-	git clone -q https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	git clone --quiet https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 	chsh -s $(which zsh) 1>/dev/null
 	echo "[!] Remember to log out and login again"
+	
+
 
 	read  -n 1 -p "[.] Press Enter to continue..."
 
