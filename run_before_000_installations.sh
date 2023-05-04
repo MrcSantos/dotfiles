6128022d00db2d27a0cd54b6a7a9ecb231d639c5
Mirco Santosuosso <mirco.santosuosso@gmail.com>
2023-05-04 13:54:44 +0200

    Update run_before_000_installations.sh

--- run_before_000_installations.sh
+++ run_before_000_installations.sh
@@ -144,12 +144,52 @@ then
 	neofetch
 fi
 
-if [ "$OS" = '"Debian"' ]
+kali() {
+	echo ""
+	echo "[-] Updating system..."
+	apt update 1>/dev/null
+	apt upgrade -y 1>/dev/null
+	apt dist-upgrade -y 1>/dev/null
+	
+	echo "[-] Installing basic tools..."
+	apt install wget unzip neofetch git tmux vim cargo 1>/dev/null
+
+	echo "[-] Installing nnn..."
+	apt install nnn 1>/dev/null
+
+	echo "[-] Installing ZShell..."
+	apt install zsh 1>/dev/null
+	chsh -s $(which zsh) $1 1>/dev/null
+	chsh -s $(which zsh) 1>/dev/null
+
+	echo "[-] Installing NerdFonts..."
+	wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hasklig.zip -O /tmp/nerd_fonts.zip 1>/dev/null
+	mkdir -p /home/$1/.local/share/fonts
+	mkdir -p /root/.local/share/fonts
+	mkdir -p /root/.config/nvim
+	unzip /tmp/nerd_fonts.zip -d /home/$1/.local/share/fonts 1>/dev/null
+	unzip /tmp/nerd_fonts.zip -d /root/.local/share/fonts 1>/dev/null
+	chown -R $1 /home/$1/.local/share/fonts
+
+	echo "[-] Installing nvim..."
+	apt install neovim 1>/dev/null
+	cargo install ripgrep 1>/dev/null
+	sh -c "git clone https://github.com/NvChad/NvChad /root/.config/nvim --depth 1" 1>/dev/null
+}
+
+if [ "$OS" = 'PRETTY_NAME="Kali GNU/Linux Rolling"' ]
 then
-	sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
-	sudo apt install neofetch git tmux vim -y
+	echo $PASSWORD | sudo -S bash -c "$(declare -f kali); kali $USER"
+
+	echo 'y' | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 1>/dev/null
+	sh -c "git clone --quiet https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" 1>/dev/null
+	echo "[!] Remember to log out and login again"
+	
+
+	sh -c "git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1" 1>/dev/null
+
+	read  -n 1 -p "[.] Press Enter to continue..."
 
 	clear
-
 	neofetch
 fi
