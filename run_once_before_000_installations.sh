08a8b5c731b1e221ec988abf5e51cb7941cded4e
Mirco Santosuosso <mirco.santosuosso@gmail.com>
2023-09-01 00:45:57 +0200

    Update run_once_before_000_installations.sh

--- run_once_before_000_installations.sh
+++ run_once_before_000_installations.sh
@@ -1,196 +1,243 @@
 #!/bin/bash
-set -eu
 
-NON_SUDO_USER=$USER
+# Controllo lancio con sudo
+if [[ $EUID -ne 0 ]]; then
+    echo "[!] Sorry, this script must be run as root, aborting..."
+    exit 1
+fi
+
+# Controllo OS
 OS=$(( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1)
-read -p "[.] Enter the sudo password: " -s PASSWORD
-echo ""
+INSTALL_COMMAND=""
+SYSTEM_DIR="/usr/local/bin"
 
+echo
+echo "[!] Installing basic tools... (This can take a while)"
+echo
 
+case $OS in
+    *Solus*)
+        INSTALL_COMMAND="eopkg install -y"
+        OS="Solus OS"
+        SYSTEM_DIR="/usr/bin"
 
-solus() {
-	echo ""
-	echo "[-] Updating system... (It may take a long time)"
-	eopkg upgrade -y &>/dev/null
-	
-	echo "[-] Installing basic tools... (It may take a long time)"
-	eopkg install -c system.devel -y &>/dev/null
-	eopkg install neofetch git git-flow tmux vim cargo -y &>/dev/null
-	
-	echo "[-] Installing NerdFonts..."
-	sh -c "git clone --quiet https://github.com/ryanoasis/nerd-fonts /opt/nerd-fonts" &>/dev/null
-	chmod a+x /opt/nerd-fonts/install.sh
-	/opt/nerd-fonts/install.sh -q -S hasklig
+        eopkg install -c system.devel -y &>/dev/null
+        eopkg install neofetch git git-flow tmux vim cargo -y &>/dev/null
+    ;;
 
-	echo "[-] Installing ZShell..."
-	eopkg install zsh -y &>/dev/null
-	chsh -s $(which zsh) $1 &>/dev/null
-	chsh -s $(which zsh) &>/dev/null
-	
-	echo "[-] Installing nnn with nerd fonts..."
-	sh -c "git clone https://github.com/jarun/nnn.git /opt/nnn --quiet" &>/dev/null
-	cd /opt/nnn
-	make O_NERD=1 &>/dev/null
-	cp /opt/nnn/nnn /bin/
+    *Kali*)
+        INSTALL_COMMAND="apt install -y"
+        OS="Kali Linux"
+        apt install -y wget unzip neofetch git tmux vim cargo &>/dev/null
+    ;;
 
-	echo "[-] Installing nvim..."
-	eopkg install neovim -y &>/dev/null
-	cargo install -q ripgrep &>/dev/null
-	sh -c "git clone --quiet https://github.com/NvChad/NvChad /root/.config/nvim --depth 1" &>/dev/null
+    *Ubuntu*)
+        INSTALL_COMMAND="apt install -y"
+        OS="Ubuntu"
+        apt install -y wget unzip neofetch git tmux vim cargo &>/dev/null
+    ;;
+
+    *Arch*)
+        INSTALL_COMMAND="pacman -S --noconfirm"
+        OS="Arch Linux (fatti una vita)"
+        pacman -S --noconfirm wget unzip neofetch git tmux vim cargo &>/dev/null
+    ;;
+
+    *Void*)
+        INSTALL_COMMAND="xbps-install"
+        OS="Void Linux (fatti una vita)"
+        xbps-install git neofetch wget gcc tmux vim cargo unzip
+    ;;
+
+    *)
+        echo "Unknown OS: $OS"
+        exit 1
+    ;;
+esac
+
+######################################################### FUNCTIONS
+
+install_nerdFonts() {
+    echo "[!] Installing Nerd Fonts..."
+    # Nerd Fonts
+    mkdir -p /usr/share/fonts &>/dev/null
+    mkdir -p /opt/nf &>/dev/null
+    cd /opt/nf &>/dev/null
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip &>/dev/null
+    unzip /opt/nf/JetBrainsMono.zip -d /usr/share/fonts &>/dev/null
+    rm -rf /opt/nf &>/dev/null
+    fc-cache -f -v &>/dev/null
+    cd &>/dev/null
 }
 
-if [ "$OS" = '"Solus"' ]
-then
-	echo $PASSWORD | sudo -S bash -c "$(declare -f solus); solus $USER"
-	sh -c "git clone --quiet https://github.com/NvChad/NvChad ~/.config/nvim --depth 1" &>/dev/null
-	echo 'y' | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &>/dev/null
-	sh -c "git clone --quiet https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" &>/dev/null
-	
-	echo ""
-	echo "[!] Remember these things:"
-	echo "    - Logout and login again"
-	echo "    - ZSH will be the default shell only for root and this user"
-	echo ""
-
-	read  -n 1 -p "[.] Press Enter to continue..."
-
-	clear
-	neofetch
-fi
-
-arch() {
-	echo ""
-	echo "[-] Updating system..."
-	pacman-key --refresh-keys
-	pacman -Sy archlinux-keyring --noconfirm && pacman -Syyu --noconfirm &>/dev/null
-	
-	echo "[-] Installing basic tools..."
-	pacman -S --noconfirm wget unzip neofetch git tmux vim cargo &>/dev/null
-
-	echo "[-] Installing nnn..."
-	pacman -S --noconfirm nnn &>/dev/null
-
-	echo "[-] Installing ZShell..."
-	pacman -S --noconfirm zsh &>/dev/null
-	chsh -s $(which zsh) $1 &>/dev/null
-	chsh -s $(which zsh) &>/dev/null
-
-	echo "[-] Installing NerdFonts..."
-	wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hasklig.zip -O /tmp/nerd_fonts.zip &>/dev/null
-	mkdir -p /home/$1/.local/share/fonts
-	mkdir -p /root/.local/share/fonts
-	mkdir -p /root/.config/nvim
-	unzip /tmp/nerd_fonts.zip -d /home/$1/.local/share/fonts &>/dev/null
-	unzip /tmp/nerd_fonts.zip -d /root/.local/share/fonts &>/dev/null
-	chown -R $1 /home/$1/.local/share/fonts
-
-	echo "[-] Installing nvim..."
-	pacman -S --noconfirm neovim &>/dev/null
-	cargo install ripgrep &>/dev/null
-	sh -c "git clone https://github.com/NvChad/NvChad /root/.config/nvim --depth 1" &>/dev/null
+install_nnn() {
+    echo "[!] Installing nnn..."
+    # nnn
+    cd /opt/ &>/dev/null
+    git clone https://github.com/jarun/nnn.git &>/dev/null
+    cd nnn &>/dev/null
+    make O_NERD=1 &>/dev/null
+    mv nnn $SYSTEM_DIR &>/dev/null
+    chmod a+x $SYSTEM_DIR/nnn &>/dev/null
+    rm -rf /opt/nnn &>/dev/null
+    cd &>/dev/null
 }
 
-if [ "$OS" = 'NAME="Arch Linux"' ]
-then
-	echo $PASSWORD | sudo -S bash -c "$(declare -f arch); arch $USER"
+install_zsh() {
+    echo "[!] Installing Zsh and all plugins..."
+    # Zsh with plugins for all users
+    $INSTALL_COMMAND zsh &>/dev/null
 
-	echo 'y' | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &>/dev/null
-	sh -c "git clone --quiet https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" &>/dev/null
-	echo "[!] Remember to log out and login again"
-	
+    zsh_path=$(command -v zsh)
 
-	sh -c "git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1" &>/dev/null
+    set_zsh_with_plugins_for_user() {
+        local username=$1
 
-	read  -n 1 -p "[.] Press Enter to continue..."
+        chsh -s "$zsh_path" "$username" &>/dev/null
 
-	clear
-	neofetch
-fi
+        [ "$username" = "root" ] && user_folder="/root" || user_folder="/home/$username"
 
-if [ "$OS" = '"Void Linux"' ]
-then
-	echo "[!] Vecchio ma fatti una vita..."
-	echo ""
-	echo "[-] Updating system..."
-	xbps-install -Su
-	
-	echo "[-] Installing basic tools..."
-	xbps-install git neofetch wget gcc tmux vim cargo unzip
+        echo 'y' | su - $username -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &>/dev/null
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $user_folder/.oh-my-zsh/custom/themes/powerlevel10k &>/dev/null
+        git clone https://github.com/zsh-users/zsh-autosuggestions.git $user_folder/.oh-my-zsh/custom/plugins/zsh-autosuggestions &>/dev/null
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $user_folder/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting &>/dev/null
 
-	echo "[-] Installing nnn..."
-	xbps-install nnn
+        sed -i 's/ZSH_THEME="[^"]*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' $user_folder/.zshrc &>/dev/null
+        sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' $user_folder/.zshrc &>/dev/null
+        echo "EDITOR=nvim" >> .zshrc
+        echo 'alias v="nvim"' >> .zshrc
+    }
 
-	echo "[-] Installing ZShell..."
-	xbps-install zsh
-	chsh -s $(which zsh)
-	echo 'y' | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-	sh -c "git clone --quiet https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-	echo "[!] Remember to log out and login again"
-
-	echo "[-] Installing NerdFonts..."
-	wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hasklig.zip -O /tmp/nerd_fonts.zip
-	mkdir -p /root/.local/share/fonts
-	mkdir -p /root/.config/nvim
-	unzip /tmp/nerd_fonts.zip -d /root/.local/share/fonts
-
-	echo "[-] Installing nvim..."
-	xbps-install neovim
-	cargo install ripgrep
-	sh -c "git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1"
-	
-	read -n 1 -p "[.] Press Enter to continue..."
-
-	clear
-
-	neofetch
-fi
-
-kali() {
-	echo ""
-	echo "[-] Updating system..."
-	apt update &>/dev/null
-	apt upgrade -y &>/dev/null
-	apt dist-upgrade -y &>/dev/null
-	
-	echo "[-] Installing basic tools..."
-	apt install wget unzip neofetch git tmux vim cargo &>/dev/null
-
-	echo "[-] Installing nnn..."
-	apt install nnn &>/dev/null
-
-	echo "[-] Installing ZShell..."
-	apt install zsh &>/dev/null
-	chsh -s $(which zsh) $1 &>/dev/null
-	chsh -s $(which zsh) &>/dev/null
-
-	echo "[-] Installing NerdFonts..."
-	wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hasklig.zip -O /tmp/nerd_fonts.zip &>/dev/null
-	mkdir -p /home/$1/.local/share/fonts
-	mkdir -p /root/.local/share/fonts
-	mkdir -p /root/.config/nvim
-	unzip /tmp/nerd_fonts.zip -d /home/$1/.local/share/fonts &>/dev/null
-	unzip /tmp/nerd_fonts.zip -d /root/.local/share/fonts &>/dev/null
-	chown -R $1 /home/$1/.local/share/fonts
-
-	echo "[-] Installing nvim..."
-	apt install neovim &>/dev/null
-	cargo install ripgrep &>/dev/null
-	sh -c "git clone https://github.com/NvChad/NvChad /root/.config/nvim --depth 1" &>/dev/null
+    set_zsh_with_plugins_for_user "root"
+    for user in $USER_LIST; do
+        set_zsh_with_plugins_for_user "$user"
+    done
 }
 
-if [ "$OS" = 'PRETTY_NAME="Kali GNU/Linux Rolling"' ]
-then
-	echo $PASSWORD | sudo -S bash -c "$(declare -f kali); kali $USER"
+install_nvchad() {
+    echo "[!] Installing Nvim with NVChad..."
+    # NVChad
+    $INSTALL_COMMAND ripgrep neovim &>/dev/null
 
-	echo 'y' | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &>/dev/null
-	sh -c "git clone --quiet https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" &>/dev/null
-	echo "[!] Remember to log out and login again"
-	
+    install_nvchad_for_user() {
+        local username=$1
 
-	sh -c "git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1" &>/dev/null
+        [ "$username" = "root" ] && user_folder="/root" || user_folder="/home/$username"
 
-	read  -n 1 -p "[.] Press Enter to continue..."
+        su - $username -c "git clone https://github.com/NvChad/NvChad $user_folder/.config/nvim --depth 1" &>/dev/null
+    }
 
-	clear
-	neofetch
-fi
+    install_nvchad_for_user "root"
+    for user in $USER_LIST; do
+        install_nvchad_for_user "$user"
+    done
+}
+
+install_ohmytmux() {
+    echo "[!] Installing OhMyTmux..."
+
+    git clone https://github.com/gpakosz/.tmux.git "/opt/.tmux" &>/dev/null
+
+    install_ohmytmux_for_user() {
+        local username=$1
+
+        [ "$username" = "root" ] && user_folder="/root" || user_folder="/home/$username"
+
+        echo 'TERM="xterm-256color"' >> $user_folder/.bashrc
+        echo 'TERM="xterm-256color"' >> $user_folder/.zshrc
+
+        mkdir -p $user_folder/.config/tmux
+
+        ln -s /opt/.tmux/.tmux.conf $user_folder/.config/tmux/tmux.conf
+        cp /opt/.tmux/.tmux.conf.local $user_folder/.config/tmux/tmux.conf.local
+    }
+
+    install_ohmytmux_for_user "root"
+    for user in $USER_LIST; do
+        install_ohmytmux_for_user "$user"
+    done
+}
+
+#create_aliases() {
+#    # TODO
+#    echo alias ssh="ssh -t -- /bin/sh -c 'tmux has-session && exec tmux attach || exec tmux' >> .zshrc
+#}
+
+
+######################################## END OF FUNCTIONS
+
+echo
+echo "██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    ███████╗██╗██████╗ "
+echo "██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    ██╔════╝██║██╔══██╗"
+echo "██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗      ███████╗██║██████╔╝"
+echo "██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝      ╚════██║██║██╔══██╗"
+echo "╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗    ███████║██║██║  ██║"
+echo " ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝    ╚══════╝╚═╝╚═╝  ╚═╝"
+echo
+echo Powered by: $OS
+echo
+
+PS3='> '
+WORKSTATION_TYPES=("Programming" "Desktop" "Hacking")
+USER_LIST=$(awk -F: '$3 >= 1000 && $7 != "/sbin/nologin" {print $1}' /etc/passwd)
+
+echo "[?] Choose the workstation type:"
+echo "0) Exit"
+
+select option in "${WORKSTATION_TYPES[@]}"; do
+    if [ "$REPLY" = "0" ]; then
+        echo "[!] Terminating program..."
+        exit 0
+    fi
+
+    echo
+    echo "[!] Setting up for $option..."
+    echo
+
+    case $option in
+        "Programming")
+            install_nerdFonts
+            install_nnn
+            install_zsh
+            install_nvchad
+            install_ohmytmux
+            #create_aliases
+            break
+        ;;
+
+        "Desktop")
+            install_nerdFonts
+            install_nnn
+            install_zsh
+            install_nvchad
+            install_ohmytmux
+            #create_aliases
+            break
+        ;;
+
+        "Hacking")
+            install_nerdFonts
+            install_nnn
+            install_zsh
+            install_nvchad
+            install_ohmytmux
+            #create_aliases
+            break
+        ;;
+
+        *)
+            echo "Invalid option $REPLY"
+        ;;
+    esac
+done
+
+clear
+neofetch
+
+echo
+echo "[!] Remember:"
+echo " -  Reboot the host"
+echo " -  Change console font to JetBrains'"
+echo " -  Change wallpaper"
+echo " -  Change keybindings to open the terminal"
+echo
