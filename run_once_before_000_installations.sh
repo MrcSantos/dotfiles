2c5ecd7ca68c8236cd3b152a639aee5cd14074fe
Mirco Santosuosso <mirco.santosuosso@gmail.com>
2024-08-01 18:50:25 +0000

    Update run_once_before_000_installations.sh

--- run_once_before_000_installations.sh
+++ run_once_before_000_installations.sh
@@ -241,3 +241,62 @@ echo " -  Change console font to JetBrains'"
 echo " -  Change wallpaper"
 echo " -  Change keybindings to open the terminal"
 echo
+
+    1  yay
+    2  git
+    3  pacman -Syu
+    4  sudo pacman -Syu
+    5  pacman -Syu git
+    6  sudo pacman -Syu git
+    7  git clone https://aur.archliux.org/librewolf-bin.git /opt/librewolf
+    8  sudo su
+    9  cd /opt/librewolf/
+   10  makepkg -si
+   11  sudo su
+   12  makepkg -si
+   13  sudo su
+   14  cd ../yay
+   15  makepkg -si
+   16  yay
+   17  yay -S librewolf-bin
+   18  mkdir -p ~/.config/alacritty/
+   19  curl -sS https://starship.rs/install.sh | sh
+   20  curl -sS https://starship.rs/install.sh | sh
+   21  pacman -Syu zsh
+   22  sudo pacman -Syu zsh
+   23  zsh
+   24  alacritty migrate
+   25  fc-list | grep "JetBrainsMono"
+   26  echo "Nerd Font Symbols:     "
+   27  vim ~/.config/starship.toml
+   28  eval "$(starship init zsh)"  # Use `zsh` or your preferred shell
+   29  eval "$(starship init zsh)"
+   30  eval "$(starship init bash)"  # Use `zsh` or your preferred shell
+   31  vim ~/.config/starship.toml
+   32  sudo su
+   33  makepkg -si
+   34  cd /opt/yay
+   35  makepkg -si
+   36  history
+[mrcsantos@arianna ~]$ sudo su
+[sudo] password for mrcsantos:
+[root@arianna mrcsantos]# history
+    1  git clone https://aur.archliux.org/librewolf-bin.git /opt/librewolf
+    2  git clone https://aur.archlinux.org/librewolf-bin.git /opt/librewolf
+    3  cd $_
+    4  ls
+    5  makepkg -si
+    6  chown mrcsantos /opt/librewolf/
+    7  pacman -Syu base-devel
+    8  git clone https://aur.archlinux.org/yay-bin.git /opt/yay
+    9  chown mrcsantos /opt/yay
+   10  mkdir -p ~/.config/alacritty/
+   11  vim ~/.config/alacritty/alacritty.yml
+   12  killall alacritty
+   13  alacritty &
+   14  alacritty migrate
+   15  cat .bash_history
+   16  cd
+   17  history
+   18  makepkg -si --noconfirm
+   19  history
